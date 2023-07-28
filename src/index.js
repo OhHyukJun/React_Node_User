56cfab8c8b21d033ed37e26565d51b8176520569
OhHyukJun <kmh7277@naver.com>
2023-07-28 11:36:35 +0900

    Feat: 로그인 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const cookieParser = require("cookie-parser");
 const { User } = require('./Model/User');
 //user 모델 불러오기
 const config = require("./config/key");
@@ -8,6 +9,8 @@ const mongoose = require('mongoose');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cookieParser());
 //더이상 body-parser는 사용하지 않기때문에 이걸 추가해야함
 const connect = async () => {
   try {
@@ -37,11 +40,36 @@ app.post('/api/users/register', async (req,res) => {
 
 app.post('api/users/login', async (req,res) => {
   try{
+    const user = new User.findOne({email: req.body.email});
     
+    if(!user){
+        return res.json({
+          loginSuccess: false,
+          message: "존재하지 않는 이메일입니다.",
+        });
+      }
+      
+      const isMatch = await user.comparePassword(req.body.password);
+
+      if(!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 일치하지 않습니다",
+        });
+      }
+
+      user.generateToken((err, user)=>{
+        if(err) return res.status(400).send(err);
+
+        res.cookie("x_auth",user.token).status(200).json({
+          loginSuccess:true,
+          userId: user._id,
+        });
+      })
   } catch(err){
-    
+    res.status(400).send(error);
   }
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
